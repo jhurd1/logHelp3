297433ae043393947b79621167bc3249b6c51704
jhurd1 <jamiehurd3@gmail.com>
2022-05-06 20:06:04 -0600

    keys established

--- src/main/java/Model/SearchResults.java
+++ src/main/java/Model/SearchResults.java
@@ -2,6 +2,10 @@ package Model;
 
 import org.hibernate.annotations.DynamicUpdate;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToOne;
 import java.util.Collections;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -17,13 +21,39 @@ public class SearchResults
     /*******************************
      * Data Members
      *******************************/
-    //https://docs.oracle.com/javase/8/docs/api/java/util/TreeSet.html
+    private Long id;
     private SortedSet s = Collections.synchronizedSortedSet(new TreeSet<String>());
     /*******************************
      * Constructors
      *******************************/
-
+    public SearchResults(Long id, SortedSet s)
+    {
+        this.id = id;
+        this.s = s;
+    }
     /*******************************
      * Accessors and Mutators
      *******************************/
+    @Column(name="RESULTS_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @OneToOne
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public SortedSet getS()
+    {
+        return s;
+    }
+
+    public void setS(SortedSet s)
+    {
+        this.s = s;
+    }
 }
